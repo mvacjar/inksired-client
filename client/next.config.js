cdeb0ffed6722dfc3b14e6246ab5d23e11610bd9
mvacjar <mvacjar@gmail.com>
2024-09-03 16:15:17 +0200

    .

--- client/next.config.js
+++ client/next.config.js
@@ -13,7 +13,7 @@
 
 // export default nextConfig;
 
-/** @type {import('next').NextConfig} **/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   images: {
@@ -23,7 +23,27 @@ const nextConfig = {
         hostname: 'inksired.s3.eu-north-1.amazonaws.com',
       },
     ],
+    // Agrega otros ajustes para imágenes si es necesario
   },
+  async redirects() {
+    return [
+      {
+        source: '/old-path',
+        destination: '/new-path',
+        permanent: true,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/proxy/:path*',
+      },
+      // Puedes agregar más reglas de reescritura según sea necesario
+    ];
+  },
+  // Agrega otras configuraciones necesarias aquí
 };
 
 export default nextConfig;
