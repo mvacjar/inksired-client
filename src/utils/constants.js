ca58382d70df2273d483b0ccbf8ae048ddc7f0ab
mvacjar <mvacjar@gmail.com>
2024-09-03 16:38:48 +0200

    add server url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-  SERVER_HOST: 'http://localhost:1337',
-  API_URL: 'http://localhost:1337/api',
+  SERVER_HOST: 'https://inksired-server-production.up.railway.app/',
+  API_URL: 'https://inksired-server-production.up.railway.app/api',
   ENDPOINTS: {
     AUTH: { REGISTER: 'auth/local/register', LOGIN: 'auth/local' },
     USERS_ME: 'users/me',
