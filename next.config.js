099516db6a4a589738841108d607b816ba58eb80
mvacjar <mvacjar@gmail.com>
2024-09-04 15:39:14 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 // /** @type {import('next').NextConfig} **/
-const nextConfig = {
-  reactStrictMode: false,
+// const nextConfig = {
+//   reactStrictMode: false,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'inksired.s3.eu-north-1.amazonaws.com',
+//       },
+//     ],
+//   },
+// };
+
+module.exports = {
   images: {
     remotePatterns: [
       {
