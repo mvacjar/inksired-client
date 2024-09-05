0cafa9ff3476b2359064b23abe1cf9e2cc54250a
mvacjar <mvacjar@gmail.com>
2024-09-05 17:00:41 +0200

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-  SERVER_HOST: 'http://localhost:1337',
-  API_URL: 'http://localhost:1337/api',
+  SERVER_HOST: 'https://inksired-server-production.up.railway.app',
+  API_URL: 'https://inksired-server-production.up.railway.app/api',
   ENDPOINTS: {
     AUTH: { REGISTER: 'auth/local/register', LOGIN: 'auth/local' },
     USERS_ME: 'users/me',
@@ -20,4 +20,3 @@ export const ENV = {
   STRIPE_TOKEN:
     'pk_test_51PigTEGYnwDtBj0M52udpwnRgpeIrC4rhAcBks8AxEAmSLJhDKIPyfvyIvIC9p19FfUn3ZQRm7JD9HozIgrEGz6N00ZxxMJ19s',
 };
-//
