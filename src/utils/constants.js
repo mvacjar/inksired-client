70509a9d88f2315b0aa982e68d28fe77f2c9e5e0
mvacjar <mvacjar@gmail.com>
2024-09-05 16:11:06 +0200

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const ENV = {
   SERVER_HOST: 'https://inksired-server-production.up.railway.app',
   API_URL: 'https://inksired-server-production.up.railway.app/api',
@@ -17,7 +21,5 @@ export const ENV = {
   },
   TOKEN: 'token',
   CART: 'cart',
-  STRIPE_TOKEN:
-    'sk_test_51PigTEGYnwDtBj0MSe8ADUVKGvJfloEU4RPwo2pNTMtObgmrnUxhbQiWZ8ElB5DMHOgMtCj19XU2xspFFRBdZ9Dw00K0Y8OQwQ',
+  STRIPE_TOKEN: process.env.STRIPE_TOKEN,
 };
-//#
