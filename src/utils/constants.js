e98c98ae96ba69e4c384502d6f5d92ed069dadba
mvacjar <mvacjar@gmail.com>
2024-09-05 16:02:33 +0200

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+import { ENV } from './env';
+
 export const ENV = {
   SERVER_HOST: 'https://inksired-server-production.up.railway.app',
   API_URL: 'https://inksired-server-production.up.railway.app/api',
@@ -18,5 +20,5 @@ export const ENV = {
   TOKEN: 'token',
   CART: 'cart',
   STRIPE_TOKEN:
-    'pk_test_51PigTEGYnwDtBj0M52udpwnRgpeIrC4rhAcBks8AxEAmSLJhDKIPyfvyIvIC9p19FfUn3ZQRm7JD9HozIgrEGz6N00ZxxMJ19s',
+    'sk_test_51PigTEGYnwDtBj0MSe8ADUVKGvJfloEU4RPwo2pNTMtObgmrnUxhbQiWZ8ElB5DMHOgMtCj19XU2xspFFRBdZ9Dw00K0Y8OQwQ',
 };
