05ddcf9170e28c046c1694b52a048e5b2f78c8ad
mvacjar <mvacjar@gmail.com>
2025-06-19 20:29:38 +0200

    New ids in homePage

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -2,24 +2,14 @@ import styles from './home.module.scss';
 import { useAuth } from '@/hooks';
 import { BasicLayout } from '@/layouts';
 import { HomeComponents } from '@/components/HomeComponents';
-import {
-  Separator,
-  BarInfo,
-  CarouselBooks,
-  BannerAd,
-  Seo,
-} from '@/components/Shared';
+import { Separator, BarInfo, CarouselBooks, BannerAd, Seo } from '@/components/Shared';
 import Footer from '@/components/Footer/Footer';
 
 export default function HomePage() {
   const { user } = useAuth();
 
-  const titleBanner = user
-    ? 'Send us your best cover for a fantasy book and...'
-    : 'Register now and get a discount!';
-  const subtitleBanner = user
-    ? 'Get 5 books from your wishlist for free!✨'
-    : 'Get a notebook for free! ✨';
+  const titleBanner = user ? 'Send us your best cover for a fantasy book and...' : 'Register now and get a discount!';
+  const subtitleBanner = user ? 'Get 5 books from your wishlist for free!✨' : 'Get a notebook for free! ✨';
   const btnTitleBanner = user ? 'Participate' : 'Sign up';
   const btnLinkBanner = user ? '/account' : '/join/sign-up';
 
@@ -49,21 +39,15 @@ export default function HomePage() {
           <Separator height={100} />
 
           <section className={styles.bannerAdContainer}>
-            <BannerAd
-              titleBanner={titleBanner}
-              subtitleBanner={subtitleBanner}
-              btnTitleBanner={btnTitleBanner}
-              btnLinkBanner={btnLinkBanner}
-              image='/images/imgAd.png'
-            />
+            <BannerAd titleBanner={titleBanner} subtitleBanner={subtitleBanner} btnTitleBanner={btnTitleBanner} btnLinkBanner={btnLinkBanner} image='/images/imgAd.png' />
           </section>
           <Separator height={50} />
 
           <section className={styles.carouselBooksContainer}>
-            <CarouselBooks genreId={2} title='Latest Fantasy Books' />
+            <CarouselBooks genreId={4} title='Latest Fantasy Books' />
           </section>
           <section className={styles.carouselBooksContainer}>
-            <CarouselBooks genreId={16} title='Latest Thriller Books' />
+            <CarouselBooks genreId={6} title='Latest Thriller Books' />
           </section>
           <Separator height={30} />
 
